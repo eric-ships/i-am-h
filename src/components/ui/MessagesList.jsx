bf17db3905bfefcb6b62ccc5b7ec23f7d6b4a654
Eric Liu <dathtohn@gmail.com>
2016-07-12 03:25:49 -0400

    update message-list ui

--- src/components/ui/MessagesList.jsx
+++ src/components/ui/MessagesList.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
 import CSSModules from 'react-css-modules'
+import React from 'react'
+import { findDOMNode } from 'react-dom'
 
 import styles from 'modules/messages-list'
 
-const MessagesList = ({ messages }) => (
-  <ul>
-    {messages.map((message, i) =>
-      <li key={i} styleName={message.fromUser ? 'message--from-user' : 'message'}>
-        {message.text}
-      </li>
-    )}
-  </ul>
-)
+class MessagesList extends React.Component {
+  static propTypes = {
+    messages: React.PropTypes.array
+  };
+
+  static defaultProps = {
+    messages: []
+  };
+
+  componentDidUpdate() {
+    const n = findDOMNode(this)
+    n.scrollTop = n.scrollHeight
+  }
+
+  render() {
+    return (
+      <ul styleName='message-list'>
+        {this.props.messages.map((message, i) =>
+          <li key={i} styleName={message.fromUser ? 'message--from-user' : 'message'}>
+            {message.text}
+          </li>
+        )}
+      </ul>
+    )
+  }
+}
 
 export default CSSModules(MessagesList, styles)
