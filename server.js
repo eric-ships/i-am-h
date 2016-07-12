13f2ebf1dee407f730d072698578bae7181abab8
Eric Liu <dathtohn@gmail.com>
2016-07-12 14:02:12 -0400

    update favicon

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 var port = process.env.PORT || 8080;
 
-app.use(favicon(path.join(__dirname, 'static','images','favicon.ico')));
+app.use(favicon(path.join(__dirname, 'static','images','favicon.png')));
 
 app.use(express.static(path.join(__dirname)));
 
